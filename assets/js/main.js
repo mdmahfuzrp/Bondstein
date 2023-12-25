03fa9d42a0142dfd6b66a3313f3039efda4671ce
Mahfuz RP <mahfuzrpsmorg@gmail.com>
2023-12-25 19:55:21 +0600

    responsive html navbar complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,70 @@
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
+/*--------- SCROLL SECTIONS ACTIVE LINK ---------*/
+const sections = document.querySelectorAll("section[id]");
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+/*--------- SEARCH ---------*/
+const search = document.getElementById("search"),
+  searchBtn = document.getElementById("search-btn"),
+  searchClose = document.getElementById("search-close");
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-            sectionTop = current.offsetTop - 50,
-            sectionId = current.getAttribute('id')
+/* Search show */
+searchBtn.addEventListener("click", () => {
+  search.classList.add("show-search");
+  document.body.style.overflow = 'hidden';
+});
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
+/* Search hidden */
+searchClose.addEventListener("click", () => {
+  search.classList.remove("show-search");
+  document.body.style.overflow = '';
+});
+
+/*--------- LOGIN ---------*/
+const login = document.getElementById('login'),
+      loginBtn = document.getElementById('login-btn'),
+      loginClose = document.getElementById('login-close')
+
+/* Login show */
+loginBtn.addEventListener('click', () =>{
+   login.classList.add('show-login')
+   document.body.style.overflow = 'hidden';
+})
+
+/* Login hidden */
+loginClose.addEventListener('click', () =>{
+   login.classList.remove('show-login')
+   document.body.style.overflow = '';
+})
+
+// Grab all navigation links
+const navLinks = document.querySelectorAll('.nav-link');
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const targetId = this.getAttribute('href');
+  const targetSection = document.querySelector(targetId);
+  
+  if (targetSection) {
+    const targetOffsetTop = targetSection.offsetTop - 50;
+    window.scrollTo({
+      top: targetOffsetTop,
+      behavior: 'smooth',
+    });
+
+    // Highlight the link immediately upon click
+    navLinks.forEach((link) => {
+      link.classList.remove('active-link');
+    });
+    this.classList.add('active-link');
+  }
 }
-window.addEventListener('scroll', scrollActive)
+
+// Add click event listeners to navigation links
+navLinks.forEach((link) => {
+  link.addEventListener('click', scrollToSection);
+});
 
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+/*--------- CHANGE BACKGROUND HEADER ---------*/
 function scrollHeader(){
     const header = document.getElementById('header')
     // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
